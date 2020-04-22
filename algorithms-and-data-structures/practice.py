70f45908319ac950265f03fa16af6fc02ccff0ce
Katerina Kossler <katerinakossler@gmail.com>
2020-04-21 21:00:47 -0700

    add matching subarrays problem

--- algorithms-and-data-structures/practice.py
+++ algorithms-and-data-structures/practice.py
@@ -103,3 +103,81 @@ class Solution: # same memory storage; faster; easier to understand
     def defangIPaddr(self, address: str) -> str:
         IP_parts = address.split('.')
         return '[.]'.join(IP_parts)
+        
+        
+# You're given strings J representing the types of stones that are jewels,
+# and S representing the stones you have.  Each character in S is a type of stone you have.  
+# You want to know how many of the stones you have are also jewels.
+#
+# The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        '''
+        takes a string of characters representing (J)ewels
+        and a string representing the overall (S)tones
+        
+        records the number of Jewels found in the set of stones
+        '''
+        num_jewels = 0
+        
+        for stone in S: # O(n)
+            if stone in J: # O(n)
+                num_jewels +=1
+                
+        return num_jewels # O(n^2) solution
+        
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        '''
+        takes a string of characters representing (J)ewels
+        and a string representing the overall (S)tones
+        
+        records the number of Jewels found in the set of stones
+        '''
+        num_jewels = 0
+        
+        # build set of jewels 
+        jewels = set()
+        for jewel in J:
+            jewels.add(jewel)
+        
+        
+        for stone in S: # O(n)
+            if stone in J: # O(1)
+                num_jewels +=1
+                
+        return num_jewels # O(n) solution
+        
+# Given an array of integers and an integer k, you need to find the total number of continuous
+# subarrays whose sum equals to k.
+
+# Example 1:
+# Input:nums = [1,1,1,1,1], k = 2
+# Output: 4
+
+# Example 2:
+# Input:nums = [1,1,-1,1,1], k = 2
+# Output: 2
+
+def find_matching_num_subarrays(nums, k): # still working on
+    '''
+    nums - array of intergers in the range [-1000,1000]
+
+    k - integer to sum to
+    '''
+    count = 0
+    for starting_idx in range(0, len(nums)):
+        current_idx = starting_idx
+        current_sum = nums[current_idx]
+        if current_sum == k:
+                count += 1
+        for current_idx in range(current_idx+1, len(nums)):
+            current_sum += nums[current_idx] 
+            if current_sum == k:
+                count += 1
+    return count
+
+print(find_matching_num_subarrays(
+    [0,0,0,0,0,0,0,0,0,0],0)) 
