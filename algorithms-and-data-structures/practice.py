593d5deb0c7977c1be27ef163c827d62c7380c23
Katerina Kossler <katerinakossler@gmail.com>
2020-04-24 20:41:26 -0700

    add decompress list of ints problem

--- algorithms-and-data-structures/practice.py
+++ algorithms-and-data-structures/practice.py
@@ -259,3 +259,30 @@ class Solution:
             # go right (and then try going left immediately after)
             root = root.right
         return visited
+        
+
+# We are given a list nums of integers representing a list compressed with 
+# run-length encoding.
+
+# Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] 
+# (with i >= 0).  For each such pair, there are freq elements with value val 
+# concatenated in a sublist. Concatenate all the sublists from left to right to 
+# generate the decompressed list.
+
+# Return the decompressed list.
+
+# Input: nums = [1,2,3,4]
+# Output: [2,4,4,4]
+# Explanation: The first pair [1,2] means we have freq = 1 and val = 2 
+# so we generate the array [2].
+# The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+# At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        decompressed_nums = []
+        for index, num in enumerate(nums):
+            if index % 2 == 1:
+                new = [num] * nums[index-1]
+                decompressed_nums.extend(new)
+        return decompressed_nums
