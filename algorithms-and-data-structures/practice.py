2e4eb1c7c440364fc8ee2faa4fb7e32305769be6
Katerina Kossler <katerinakossler@gmail.com>
2020-04-24 22:04:26 -0700

    excel column title problem

--- algorithms-and-data-structures/practice.py
+++ algorithms-and-data-structures/practice.py
@@ -286,3 +286,51 @@ class Solution:
                 new = [num] * nums[index-1]
                 decompressed_nums.extend(new)
         return decompressed_nums
+  
+# Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+# For example:
+
+#     A -> 1
+#     B -> 2
+#     C -> 3
+#     ...
+#     Z -> 26
+#     AA -> 27
+#     AB -> 28 
+#     ...      
+
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        columns = {
+            'A':1,
+            'B':2,
+            'C':3,
+            'D':4,
+            'E':5,
+            'F':6,
+            'G':7,
+            'H':8,
+            'I':9,
+            'J':10,
+            'K':11,
+            'L':12,
+            'M':13,
+            'N':14,
+            'O':15,
+            'P':16,
+            'Q':17,
+            'R':18,
+            'S':19,
+            'T':20,
+            'U':21,
+            'V':22,
+            'W':23,
+            'X':24,
+            'Y':25,
+            'Z':26}
+        column = 0
+        for char in s:
+            column *= 26
+            column += columns[char]
+        return column
