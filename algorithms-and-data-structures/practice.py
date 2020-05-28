28dc99846965a69af99e78996dc1824cdbac6655
Katerina Kossler <katerinakossler@gmail.com>
2020-05-28 14:57:02 -0700

    add exception throwing/handling problems

--- algorithms-and-data-structures/practice.py
+++ algorithms-and-data-structures/practice.py
@@ -1243,3 +1243,32 @@ for i in range(T):
     data=int(input())
     head=mylist.insert(head,data)    
 mylist.display(head); 
+
+
+# Two Exception Throwing / Handling Practice problems
+# Exception Handling
+S = input().strip()
+try:
+    intS = int(S)
+    print(intS)
+except:
+    print('Bad String')
+# Exception Throwing
+class Calculator:
+    def __init__(self):
+        pass
+    def power(self,n,p):
+        if n < 0 or p < 0:
+            raise Exception('n and p should be non-negative')
+        else:
+            return n**p
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e) 
